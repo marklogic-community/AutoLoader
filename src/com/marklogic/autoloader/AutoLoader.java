6829db7033e4f8fccf2e985d674624132c593bcc
Clark Richey <clark.richey@marklogic.com>
2008-08-12 20:47:38 +0000

    added license
    git-svn-id: http://developer.marklogic.com/svn/AutoLoader/trunk@861 e04f4502-82db-0310-b1af-f799f365da79

--- src/com/marklogic/autoloader/AutoLoader.java
+++ src/com/marklogic/autoloader/AutoLoader.java
@@ -1,5 +1,19 @@
 package com.marklogic.autoloader;
 
+/*
+Copyright 2002-2008 Mark Logic Corporation. All Rights Reserved.
+
+This software is licensed to you under the terms and conditions
+
+specified at http://www.apache.org/licenses/LICENSE-2.0. If you
+
+do not agree to those terms and conditions, you must cease use of
+
+and destroy any copies of this software that you have downloaded.
+*/
+
+
+
 import org.sadun.util.polling.DirectoryPoller;
 
 import java.io.File;
